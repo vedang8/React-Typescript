673ed390265db3cafb4a2c190ebc53c9fbc96a89
Vedang <vedang2607@gmail.com>
2025-02-08 23:43:51 +0530

    Basic Props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,8 +1,33 @@
 import { Greet } from './components/Greet'
+import {Person} from './components/Person'
+import { PersonList } from './components/PersonList'
 
 function App() {
+  const personName = {
+    first: 'Vedang',
+    last: 'Joshi'
+  }
+
+  const nameList = [
+    {
+      first: 'Vedang',
+      last: 'Joshi'
+    },
+    {
+      first: 'Jay',
+      last: 'Patel'
+    },
+    {
+      first: 'Dhruvin',
+      last: 'Kavathiya'
+    }
+  ]
   return (
-      <Greet name='Vedang'/>    
+      <>
+        <Greet name='Vedang' messageCount={20} isLoggedIn={false} />
+        <Person name={personName} />
+        <PersonList names={nameList}/>
+      </>
   )
 }
 
--- react-typescript-demo/src/components/Greet.tsx
+++ react-typescript-demo/src/components/Greet.tsx
@@ -1,5 +1,7 @@
 type GreetProps = {
     name: string
+    messageCount: number
+    isLoggedIn: boolean
 }
 // if we pass any other data type other than string 
 // than it will give us compile time error
@@ -7,8 +9,12 @@ export const  Greet = (props: GreetProps) => {
     return (
         <div>
             <h2>
-                Welcome ${props.name}! 
-                you have 10 Unread messages
+                {props.isLoggedIn ? 
+                `Welcome ${props.name}! 
+                you have ${props.messageCount} Unread messages`
+                : 
+                `Please login to continue`}
+                
             </h2>
         </div>
     )   
